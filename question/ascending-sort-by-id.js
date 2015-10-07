87491402cd44760b7d4087c0fb1dbf25fe01a42e
sato <sato@tsumikiinc.com>
2015-10-07 13:55:24 +0900

    add ascending-sort-by-id

---
+++ question/ascending-sort-by-id.js
@@ -0,0 +1,18 @@
+/*
+  [
+    { id: 4, name: 'sugar' },
+    { id: 2, name: 'shin' },
+    { id: 7, name: 'shin' },
+    ...
+  ]
+
+  上記のようなプロパティをもつオブジェクトを要素にもつ配列を引数にとり、
+  `id` の値で昇順でオブジェクトをソートした配列の新しい参照を返す
+
+  @param {Array} array
+  @returns {Array}
+*/
+
+export default function(array) {
+
+}
---
+++ test/ascending-sort-by-id.js
@@ -0,0 +1,80 @@
+import assert from 'power-assert';
+import func from '../question/ascending-sort-by-id';
+
+describe('ascending-sort-by-id', () => {
+
+  it('is a function', () => {
+    assert.deepEqual(typeof func, 'function', 'ascending-sort-by-id.js is a function');
+  });
+
+  it('argument is immutable', () => {
+    const data = [
+      {
+        id: 0,
+        name: 'other'
+      },
+      {
+        id: 4,
+        name: 'other'
+      },
+      {
+        id: 1,
+        name: 'other'
+      }
+    ];
+
+    func(data);
+
+    const expected = [
+      {
+        id: 0,
+        name: 'other'
+      },
+      {
+        id: 4,
+        name: 'other'
+      },
+      {
+        id: 1,
+        name: 'other'
+      }
+    ];
+
+    assert.deepEqual(data, expected);
+  });
+
+  it('case 1', () => {
+    const actual = func([
+      {
+        id: 2,
+        name: 'other'
+      },
+      {
+        id: 10,
+        name: 'other'
+      },
+      {
+        id: 4,
+        name: 'other'
+      }
+    ]);
+
+    const expected = [
+      {
+        id: 2,
+        name: 'other'
+      },
+      {
+        id: 4,
+        name: 'other'
+      },
+      {
+        id: 10,
+        name: 'other'
+      }
+    ];
+
+    assert(actual === expected);
+  });
+
+});
