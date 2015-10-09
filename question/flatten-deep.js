89687f52a28570742598ee7b1000aab54c242e67
sugarshin <shinsugar@gmail.com>
2015-10-09 16:26:30 +0900

    add flatten-deep

---
+++ question/flatten-deep.js
@@ -0,0 +1,18 @@
+/*
+  [
+    [],
+    [[[]]],
+    [[]]
+    ...
+  ]
+
+  多次元配列を引数に受け取り、
+  1次元配列に変換した配列の新しい参照を返す
+
+  @param {Array} array
+  @returns {Array}
+*/
+
+export default function(array) {
+
+}
---
+++ test/flatten-deep.js
@@ -0,0 +1,44 @@
+import assert from 'power-assert';
+import func from '../question/flatten-deep';
+
+describe('flatten-deep', () => {
+
+  it('is a function', () => {
+    assert.deepEqual(typeof func, 'function', 'flatten-deep.js is a function');
+  });
+
+  it('argument is immutable', () => {
+    const data = [
+      [[]]
+      [],
+      [[[]]]
+    ];
+
+    func(data);
+
+    const expected = [
+      [[]]
+      [],
+      [[[]]]
+    ];
+
+    assert.deepEqual(data, expected, 'argument is immutable');
+  });
+
+  it('case 1', () => {
+    const actual = func([
+      [2[3, 5], '']
+      [4, 5, 7, 8, {}, () => {}],
+      [1, [[4, false]]true, null]
+    ]);
+
+    const expected = [
+      2, 3, 5, '',
+      4, 5, 7, 8, {}, () => {},
+      1, 4, false, true, null
+    ];
+
+    assert.deepEqual(actual, expected, 'case 1');
+  });
+
+});
