a4f6d6121a3905ef45f8dd4a040fc2fa50b330f3
sato <sato@tsumikiinc.com>
2015-10-09 19:42:02 +0900

    add find-by.js

---
+++ question/find-by.js
@@ -0,0 +1,14 @@
+/*
+  オブジェクトを要素に持つ配列を受け取り、
+  第２引数で渡されたオブジェクトのプロパティと値が
+  すべて一致するオブジェクトの新しい参照を返す
+  見つからない場合は`null`を返す
+
+  @param {Array} array
+  @param {Object} object
+  @returns {Object}
+*/
+
+export default function(array, object) {
+
+}
