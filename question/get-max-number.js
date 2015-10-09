c0f91116abccb46cf324d8a8e4fdbc7f52fc0043
sugarshin <shinsugar@gmail.com>
2015-10-10 01:12:28 +0900

    get-max-number.js

--- question/get-max-number.js
+++ question/get-max-number.js
@@ -7,5 +7,19 @@
 */
 
 export default function(array) {
-
+  return Math.max(...permute(array).map(array => +array.join('')));
 }
+
+function permute(array, permuted = [], usedCharacters = []) {
+  array.forEach((v, i) => {
+    let el = array.splice(i, 1)[0];
+    usedCharacters.push(el);
+    if (array.length === 0) {
+      permuted.push(usedCharacters.slice());
+    }
+    permute(array, permuted, usedCharacters);
+    array.splice(i, 0, el);
+    usedCharacters.pop();
+  });
+  return permuted
+};
