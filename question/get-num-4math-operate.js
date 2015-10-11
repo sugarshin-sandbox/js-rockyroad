efd6f36aef5ab6de418e64ec0717746a3f8dd4a9
sugarshin <shinsugar@gmail.com>
2015-10-11 15:40:19 +0900

    get-num-4math-operate.js

--- question/get-num-4math-operate.js
+++ question/get-num-4math-operate.js
@@ -18,5 +18,35 @@
 */
 
 export default function(number, object) {
+  const result = objectReduce(object, (returnValue, val, key) => {
+    const operator = mapOperator(key);
+    if (Array.isArray(val)) {
+      return val.reduce((result, num) => {
+        return eval(`${result} ${operator} ${num}`);
+      }, returnValue);
+    }
 
+    return eval(`${returnValue} ${operator} ${val}`);
+  }, number);
+
+  return Math.floor(result);
+}
+
+function objectReduce(object, callback, initialValue, thisArg = null) {
+  return Object.keys(object).reduce((returnValue, currentKey, i) => {
+    return callback.call(thisArg, returnValue, object[currentKey], currentKey, i, object);
+  }, initialValue);
+}
+
+function mapOperator(keyword) {
+  switch(keyword) {
+  case 'add':
+    return '+';
+  case 'sub':
+    return '-';
+  case 'multi':
+    return '*';
+  case 'div':
+    return '/';
+  }
 }
