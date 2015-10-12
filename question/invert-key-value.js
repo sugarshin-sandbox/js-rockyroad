78ae3a5ca4a4bee49882a3bf12f147f0e9978e47
Ryota Hirano <hirano@tsumikiinc.com>
2015-10-13 01:12:49 +0900

    [add] add concat-keys, invert-key-value

---
+++ question/invert-key-value.js
@@ -0,0 +1,24 @@
+/*
+  {
+    a: 100,
+    b: 10,
+    c: 101,
+    d: 10
+  }
+
+  上記のような、オブジェクトのkeyと値を逆にして新しいオブジェクトを返す。
+  上記の場合は、
+    {
+      10: ['b', 'd'],
+      100: ['a'],
+      101: ['c']
+    }
+  というオブジェクトが返る。
+
+  @param {Object} object
+  @returns {Object}
+*/
+
+export default function(object) {
+
+}
