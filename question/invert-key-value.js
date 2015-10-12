2f5e19eea41fd19068ef7b7a6e07015b192f8bfb
sugarshin <shinsugar@gmail.com>
2015-10-13 03:26:54 +0900

    passed invert-key-value.js

--- question/invert-key-value.js
+++ question/invert-key-value.js
@@ -18,7 +18,28 @@
   @param {Object} object
   @returns {Object}
 */
-
+import 'babel-core/polyfill';
 export default function(object) {
+  return objectReduce(object, (result, currentVal, currentKey, i, keys) => {
+    if (result[currentVal]) {
+      result[currentVal].add(currentKey);
+    } else {
+      result[currentVal] = new Set([currentKey]);
+    }
 
+    if (i === keys.length - 1) {
+      return objectReduce(result, (ret, val, key) => {
+        ret[key] = Array.from(val);
+        return ret;
+      }, {});
+    }
+
+    return result;
+  }, {});
+}
+
+function objectReduce(object, callback, initialValue, thisArg = null) {
+  return Object.keys(object).reduce((returnValue, currentKey, i, keys) => {
+    return callback.call(thisArg, returnValue, object[currentKey], currentKey, i, keys, object);
+  }, initialValue);
 }
