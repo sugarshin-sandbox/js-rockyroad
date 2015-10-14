a2c99d86eca026f6b33b516e8a0b4edbee46d099
sato <sato@tsumikiinc.com>
2015-10-14 11:21:54 +0900

    typo question/concat-keys.js (｀･ω･´)ゞ

--- question/concat-keys.js
+++ question/concat-keys.js
@@ -2,16 +2,17 @@
   [
     { animal: 'tiger', fruits: 'apple' },
     { animal: 'flamingo', fruits: 'grape' },
-    { animal: 'elephant', fruits: 'peach' },
-    ...
+    { animal: 'elephant', fruits: 'peach' }
   ]
 
   上記のような、配列に含まれるオブジェクトのkeyごとに値をまとめた新しい配列を返す。
   また、返す値の各keyに対する配列は昇順でソートする。
   上記の場合は、
   [
-    { animal: ['elephant', 'flamingo', 'tiger']},
-    { fruits: ['apple', 'grape', 'peach']},
+    {
+      animal: ['elephant', 'flamingo', 'tiger'],
+      fruits: ['apple', 'grape', 'peach']
+    }
   ]
   という配列が返る。
 
